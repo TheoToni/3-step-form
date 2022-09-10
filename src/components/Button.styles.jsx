fd6f78cfddf6f0ddfbd7f506df1793180da163f0
TheoToni <32997269+TheoToni@users.noreply.github.com>
2022-09-10 12:56:18 +0200

    Implementing Styled Components

---
+++ src/components/Button.styles.jsx
@@ -0,0 +1,21 @@
+import styled from "styled-components";
+
+export const StyledButton = styled.button`
+  background-image: linear-gradient(
+    to right,
+    #ff512f 0%,
+    #f09819 51%,
+    #ff512f 100%
+  );
+  margin: 10px;
+  padding: 15px 45px;
+  text-align: center;
+  text-transform: uppercase;
+  transition: 0.5s;
+  background-size: 200% auto;
+  color: white;
+  box-shadow: 0 0 20px #eee;
+  border-radius: 10px;
+  display: block;
+  outline: none;
+`;
